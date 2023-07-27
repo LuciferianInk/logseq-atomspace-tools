e9b99750e8d69a37d675e5bf2e62f547b0ace223
Luciferian Ink <LuciferianInk@protonmail.com>
2023-07-26 23:12:05 -0500

    minor updates

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
-## Hello World Sample
+## AtomSpace Tools
 
-This is a Hello World sample that just get you started :)
-
-### Demo
-
-![demo](./demo.gif)
-
-### API
-
-[![npm version](https://badge.fury.io/js/%40logseq%2Flibs.svg)](https://badge.fury.io/js/%40logseq%2Flibs)
-
-##### Logseq
-
-- `ready (callback?: (e: any) => void | {}): Promise<any>`
-
-##### Logseq.App
-
-- `showMsg: (content: string, status?: 'success' | 'warning' | string) => void`
-
-### Running the Sample
-
-- `Load unpacked plugin` in Logseq Desktop client.
+An interface for building a personal knowledge base within the OpenCog AtomSpace. Think of it like Google Blockly, or Scratch; it is a simple, block-based ecosystem that leverages LogSeq structures to build, test, and deploy AtomSpace types.
--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ function main() {
             description: 'The endpoint of the CogServer you will use.'
         }
     ])
-    const socket = openSocket(`${logseq.settings.cogServer}/json`)
+    const socket = openSocket(`${logseq.settings.cogServer}/scm`)
+    logseq.App.registerUIItem('toolbar', {
+        key: 'opencog',
+        template: `<button title="OpenCog" class="button icon inline"><span class="ui__icon ti ls-icon-settings "><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+        <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+      </svg></span></button>`
+    })
+    // logseq.provideUI({
+    //     key: 'open-calendar',
+    //     path: '#search',
+    //     template: `
+    //      <a data-on-click="openCalendar" onclick="alert('abc')' style="opacity: .6; display: inline-flex; padding-left: 3px;'>
+    //        <i class="iconfont icon-Calendaralt2"></i>
+    //      </a>
+    //     `
+    // })
 }
 
 // Open the socket
@@ -19,23 +36,40 @@ function openSocket(url) {
         socket.addEventListener('open', openConnection)
         socket.addEventListener('close', closeConnection)
         socket.addEventListener('message', readReply)
-        socket.addEventListener('error', reportError)
+        socket.addEventListener('error', readReply)
+        return socket
     } catch (err) {
+        console.error(err)
         logseq.UI.showMsg(err.toString())
     }
 }
 
+// opened websocket callback
 function openConnection() {
-    logseq.UI.showMsg('Successfully opened a connection to the CogServer!')
+    logseq.UI.showMsg(
+        'Successfully opened a connection to the CogServer!',
+        'success'
+    )
+    sendMessage(socket)
 }
 
+// closed websocket callback
 function closeConnection() {
-    logseq.UI.showMsg('Lost connection to the CogServer!')
+    logseq.UI.showMsg('Lost connection to the CogServer!', 'warning')
 }
 
-// Display the reply message
+// read the websocket reply
 function readReply(event) {
     logseq.UI.showMsg(event.data)
+}
+
+// send websocket message
+function sendMessage(socket, message = '(display "Hello, World!\n")') {
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(message)
+    } else {
+        logseq.UI.showMsg('Socket not ready!')
+    }
 }
 
 // bootstrap
