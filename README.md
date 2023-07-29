c8c2bec23d90127c178b5b886f9ae024b022e92c
Luciferian Ink <LuciferianInk@protonmail.com>
2023-07-28 22:16:40 -0500

    save state

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-## AtomSpace Tools
+## Unigra.ph
 
 An interface for building a personal knowledge base within the OpenCog AtomSpace. Think of it like Google Blockly, or Scratch; it is a simple, block-based ecosystem that leverages LogSeq structures to build, test, and deploy AtomSpace types.
+
+### Supported Graphs
+
+1. OpenCog AtomSpace
+2. GUN.js
+3. IPFS
+4. DataScript/Logseq
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let socket
+
 // plugin entrypoint
 function main() {
     logseq.useSettingsSchema([
@@ -9,23 +11,14 @@ function main() {
             description: 'The endpoint of the CogServer you will use.'
         }
     ])
-    const socket = openSocket(`${logseq.settings.cogServer}/scm`)
-    logseq.App.registerUIItem('toolbar', {
-        key: 'opencog',
-        template: `<button title="OpenCog" class="button icon inline"><span class="ui__icon ti ls-icon-settings "><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
-        <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-      </svg></span></button>`
-    })
-    // logseq.provideUI({
-    //     key: 'open-calendar',
-    //     path: '#search',
-    //     template: `
-    //      <a data-on-click="openCalendar" onclick="alert('abc')' style="opacity: .6; display: inline-flex; padding-left: 3px;'>
-    //        <i class="iconfont icon-Calendaralt2"></i>
-    //      </a>
-    //     `
+    openSocket(`${logseq.settings.cogServer}/scm`)
+    // logseq.App.registerUIItem('toolbar', {
+    //     key: 'opencog',
+    //     template: `<button title="OpenCog" class="button icon inline"><span class="ui__icon ti ls-icon-settings "><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    //     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    //     <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+    //     <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+    //   </svg></span></button>`
     // })
 }
 
@@ -34,7 +27,7 @@ function openSocket(url) {
     try {
         socket = new WebSocket(url)
         socket.addEventListener('open', openConnection)
-        socket.addEventListener('close', closeConnection)
+        socket.addEventListener('close', retryConnection)
         socket.addEventListener('message', readReply)
         socket.addEventListener('error', readReply)
         return socket
@@ -50,26 +43,62 @@ function openConnection() {
         'Successfully opened a connection to the CogServer!',
         'success'
     )
-    sendMessage(socket)
+    handleTransactions()
 }
 
 // closed websocket callback
-function closeConnection() {
-    logseq.UI.showMsg('Lost connection to the CogServer!', 'warning')
+async function retryConnection() {
+    logseq.UI.showMsg(
+        'Cannot connect to the CogServer! Trying again...',
+        'warning'
+    )
+    await delay(5000)
+    openSocket(`${logseq.settings.cogServer}/scm`)
 }
 
 // read the websocket reply
 function readReply(event) {
     logseq.UI.showMsg(event.data)
+    console.log(event.data)
 }
 
 // send websocket message
-function sendMessage(socket, message = '(display "Hello, World!\n")') {
+function sendMessage(message = `(cog-get-atoms 'Atom #t)`) {
     if (socket.readyState === WebSocket.OPEN) {
         socket.send(message)
     } else {
         logseq.UI.showMsg('Socket not ready!')
     }
+}
+
+// delay execution
+const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+
+// handle database transaction events
+function handleTransactions() {
+    logseq.DB.onChanged((e) => {
+        console.log(e)
+        if (e.txMeta.skipRefresh) return
+        if (
+            e.txMeta.outlinerOp === 'saveBlock' ||
+            e.txMeta.outlinerOp === 'insertBlocks'
+        ) {
+            if (e.blocks[0].content) {
+                for (const data of e.txData) {
+                    if (data[1] !== 'content') continue
+                    if (!data[4]) {
+                        // Deprecated previous atoms
+                        sendMessage(
+                            `(cog-set-atom-attribute! "${data[2]}" 'deprecated #t))`
+                        )
+                    } else if (data[4]) {
+                        // Create new atoms
+                        sendMessage(`(Concept "${data[2]}")`)
+                    }
+                }
+            }
+        }
+    })
 }
 
 // bootstrap
